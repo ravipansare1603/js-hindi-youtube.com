9f0660ea0b33c19c5051fb4072b7830c6e5c10f8
ravipansare1603 <ravipansare1603@gmail.com>
2026-01-06 17:47:14 +0000

    arrays implimentation done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,40 @@
+// Arrays
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["allu", "nagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[3]);
+// console.log(myHeros[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(6)
+// myArr.shift()
+
+// console.log(myArr.includes(299));
+// console.log(myArr.indexOf(2));
+
+// const newArr = myArr.join()
+
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+// Slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 3)
+
+console.log("C ", myArr);
+console.log(myn2);
