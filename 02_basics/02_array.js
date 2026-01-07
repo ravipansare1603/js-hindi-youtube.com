021983f3961812d639f9ff111a9867aaa700d4b6
ravipansare1603 <ravipansare1603@gmail.com>
2026-01-07 14:40:38 +0000

    array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,7 @@
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc_heros = ["superman", "Flash", "batman"]
+
+marvel_heros.push(dc_heros)
+
+console.log(marvel_heros);
+console.log(marvel_heros[3][1]);
