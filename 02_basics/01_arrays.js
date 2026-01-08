11d795e3dc4bdbd88a2322f6f95af5ce7e059660
ravipansare1603 <ravipansare1603@gmail.com>
2026-01-08 17:02:42 +0000

    array completed

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -27,14 +27,15 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // Slice, splice
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
-console.log(myn1);
+// const myn1 = myArr.slice(1, 3)
+// console.log(myn1);
 
-console.log("B ", myArr);
+// console.log("B ", myArr);
 
-const myn2 = myArr.splice(1, 3)
+// const myn2 = myArr.splice(1, 3)
 
-console.log("C ", myArr);
-console.log(myn2);
+// console.log("C ", myArr);
+// console.log(myn2);
+
