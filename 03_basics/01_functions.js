0d7b43a90ffd4abdb46e8081328f61fb941195c4
ravipansare1603 <ravipansare1603@gmail.com>
2026-01-13 17:25:06 +0000

    functions config done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,4 +35,38 @@ function loginUserMassege(username = "sam"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMassege("pravin"))
+// console.log(loginUserMassege("pravin"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500))
+
+
+const user = {
+    username: "pravin",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+    
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 333
+})
+
+
+const myNewArray = [200, 400, 700, 900]
+
+function returnsecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnsecondValue(myNewArray));
+console.log(returnsecondValue([200, 400, 600]));
