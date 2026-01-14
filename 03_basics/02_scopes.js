15b3f077d2fbc63fc06e4dcef214b29a39f0fdfa
ravipansare1603 <ravipansare1603@gmail.com>
2026-01-14 17:30:27 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,13 +1,59 @@
 // var c = 300
-let a = 300
+const b = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER:", a);
+    // console.log("INNER:", b);
     
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+function one(){
+    const username = "pravin"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "pravin"
+    if (username === "pravin") {
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+
+
+// +++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++
+
+function addone(num){
+    return num + 1
+}
+
+addone(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
